b7522fedc2789144c90e8a15c2e6fd0bdf517eff
Oliver Sturm <oliver@oliversturm.com>
2020-01-31 14:13:13 +0000

    phonegap

---
+++ phonegap/sudokuSolver/platforms/browser/www/precache-manifest.72513db5e2615c6fda09472752f26407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f491105b02a2f1790cc50401390a49",
+    "url": "./3226d57e9921f8ebde03.worker.js"
+  },
+  {
+    "revision": "56ac47598896804e655c6586e8f3201a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4506014f96e9d5ca9878",
+    "url": "./static/css/main.b0176b8e.chunk.css"
+  },
+  {
+    "revision": "f42d81b995b243de5d04",
+    "url": "./static/js/2.1b39a2ef.chunk.js"
+  },
+  {
+    "revision": "4506014f96e9d5ca9878",
+    "url": "./static/js/main.882e5a05.chunk.js"
+  },
+  {
+    "revision": "06a9e85369872f00469b",
+    "url": "./static/js/runtime-main.f355b94c.js"
+  }
+]);
